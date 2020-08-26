7ef941071b8c0b131258e956f6eb0035ad34bd3c
nikogoli <49331838+nikogoli@users.noreply.github.com>
2020-08-26 11:53:33 +0900

    Update DATA_PT_vertex_colors.py
    各サブモジュールにおいて
    
    ・追加先の変更に合わせ、コメントを変更
    
    ・クラスの登録・登録解除の関数を追加：
    　register(), unregister()
    
    ・プリファレンスのRNA名変更への対処：
    　contxt.user_preferences　→　context.preferences
    
    ・addon名の検索失敗への対処：
    　Scramble Addon　→　Blender-Scramble-Addon-master

--- DATA_PT_vertex_colors.py
+++ DATA_PT_vertex_colors.py
@@ -155,12 +155,32 @@ class SubMenu(bpy.types.Menu):
 		self.layout.operator(AddVertexColorSelectedObject.bl_idname, icon='PLUGIN')
 
 ################
+# クラスの登録 #
+################
+
+classes = [
+	MoveActiveVertexColor,
+	VertexColorSet,
+	AddVertexColorSelectedObject,
+	SubMenu
+]
+
+def register():
+	for cls in classes:
+		bpy.utils.register_class(cls)
+
+def unregister():
+	for cls in classes:
+		bpy.utils.unregister_class(cls)
+
+
+################
 # メニュー追加 #
 ################
 
 # メニューのオン/オフの判定
 def IsMenuEnable(self_id):
-	for id in bpy.context.user_preferences.addons["Scramble Addon"].preferences.disabled_menu.split(','):
+	for id in bpy.context.preferences.addons["Blender-Scramble-Addon-master"].preferences.disabled_menu.split(','):
 		if (id == self_id):
 			return False
 	else:
@@ -177,5 +197,5 @@ def menu(self, context):
 				sub.operator(MoveActiveVertexColor.bl_idname, icon='TRIA_DOWN', text="").mode = 'DOWN'
 				row.operator(VertexColorSet.bl_idname, icon='BRUSH_DATA', text="Paint Out")
 		row.menu(SubMenu.bl_idname, icon='PLUGIN')
-	if (context.user_preferences.addons["Scramble Addon"].preferences.use_disabled_menu):
+	if (context.preferences.addons["Blender-Scramble-Addon-master"].preferences.use_disabled_menu):
 		self.layout.operator('wm.toggle_menu_enable', icon='CANCEL').id = __name__.split('.')[-1]
