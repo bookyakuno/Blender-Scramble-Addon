750aac13e0514bd7699c1446677be14f39587173
nikogoli <49331838+nikogoli@users.noreply.github.com>
2020-08-26 11:54:21 +0900

    Update INFO_HT_header.py
    各サブモジュールにおいて
    
    ・追加先の変更に合わせ、コメントを変更
    
    ・クラスの登録・登録解除の関数を追加：
    　register(), unregister()
    
    ・プリファレンスのRNA名変更への対処：
    　contxt.user_preferences　→　context.preferences
    
    ・addon名の検索失敗への対処：
    　Scramble Addon　→　Blender-Scramble-Addon-master

--- INFO_HT_header.py
+++ INFO_HT_header.py
@@ -13,7 +13,7 @@ import bpy
 
 # メニューのオン/オフの判定
 def IsMenuEnable(self_id):
-	for id in bpy.context.user_preferences.addons["Scramble Addon"].preferences.disabled_menu.split(','):
+	for id in bpy.context.preferences.addons["Blender-Scramble-Addon-master"].preferences.disabled_menu.split(','):
 		if (id == self_id):
 			return False
 	else:
@@ -23,5 +23,5 @@ def IsMenuEnable(self_id):
 def menu_prepend(self, context):
 #	if (IsMenuEnable(__name__.split('.')[-1])):
 #		self.layout.operator('wm.load_last_file', icon='RECOVER_LAST', text="")
-	if (context.user_preferences.addons["Scramble Addon"].preferences.use_disabled_menu):
+	if (context.preferences.addons["Blender-Scramble-Addon-master"].preferences.use_disabled_menu):
 		self.layout.operator('wm.toggle_menu_enable', icon='CANCEL').id = __name__.split('.')[-1]
