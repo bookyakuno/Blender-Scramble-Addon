a31715c3026c0e053b3bb865883f9e10683f49e6
nikogoli <49331838+nikogoli@users.noreply.github.com>
2020-08-26 11:59:02 +0900

    Update VIEW3D_MT_snap.py
    各サブモジュールにおいて
    
    ・追加先の変更に合わせ、コメントを変更
    
    ・クラスの登録・登録解除の関数を追加：
    　register(), unregister()
    
    ・プリファレンスのRNA名変更への対処：
    　contxt.user_preferences　→　context.preferences
    
    ・addon名の検索失敗への対処：
    　Scramble Addon　→　Blender-Scramble-Addon-master

--- VIEW3D_MT_snap.py
+++ VIEW3D_MT_snap.py
@@ -61,12 +61,31 @@ class Move3DCursorFar(bpy.types.Operator):
 		return {'FINISHED'}
 
 ################
+# クラスの登録 #
+################
+
+classes = [
+	SnapMesh3DCursor,
+	Move3DCursorToViewLocation,
+	Move3DCursorFar
+]
+
+def register():
+	for cls in classes:
+		bpy.utils.register_class(cls)
+
+def unregister():
+	for cls in classes:
+		bpy.utils.unregister_class(cls)
+
+
+################
 # メニュー追加 #
 ################
 
 # メニューのオン/オフの判定
 def IsMenuEnable(self_id):
-	for id in bpy.context.user_preferences.addons["Scramble Addon"].preferences.disabled_menu.split(','):
+	for id in bpy.context.preferences.addons["Blender-Scramble-Addon-master"].preferences.disabled_menu.split(','):
 		if (id == self_id):
 			return False
 	else:
@@ -79,6 +98,6 @@ def menu(self, context):
 		self.layout.operator(Move3DCursorToViewLocation.bl_idname, text="Cursor => View Positon", icon="PLUGIN")
 		self.layout.operator(Move3DCursorFar.bl_idname, text="Hide Cursor (Move Far)", icon="PLUGIN")
 		self.layout.operator(SnapMesh3DCursor.bl_idname, text="Cursor => Mesh surface", icon="PLUGIN")
-	if (context.user_preferences.addons["Scramble Addon"].preferences.use_disabled_menu):
+	if (context.preferences.addons["Blender-Scramble-Addon-master"].preferences.use_disabled_menu):
 		self.layout.separator()
 		self.layout.operator('wm.toggle_menu_enable', icon='CANCEL').id = __name__.split('.')[-1]
