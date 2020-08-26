833bfbca51c0a6a123e54c2250cbc5d3947244dd
nikogoli <49331838+nikogoli@users.noreply.github.com>
2020-08-26 11:54:52 +0900

    Update INFO_MT_render.py
    各サブモジュールにおいて
    
    ・追加先の変更に合わせ、コメントを変更
    
    ・クラスの登録・登録解除の関数を追加：
    　register(), unregister()
    
    ・プリファレンスのRNA名変更への対処：
    　contxt.user_preferences　→　context.preferences
    
    ・addon名の検索失敗への対処：
    　Scramble Addon　→　Blender-Scramble-Addon-master

--- INFO_MT_render.py
+++ INFO_MT_render.py
@@ -1,5 +1,5 @@
-# 「情報」エリア > 「レンダー」メニュー
-# "Info" Area > "Render" Menu
+# 「トップバー」エリア > 「レンダー」メニュー
+# "TOPBAR" Area > "Render" Menu
 
 import bpy
 import sys, subprocess
@@ -199,12 +199,38 @@ class SubsurfMenu(bpy.types.Menu):
 		self.layout.operator(SyncAllSubsurfRenderLevels.bl_idname, text="Sync Preview Value", icon="PLUGIN")
 
 ################
+# クラスの登録 #
+################
+
+classes = [
+	SetRenderResolutionPercentage,
+	SetRenderSlot,
+	ToggleThreadsMode,
+	SetAllSubsurfRenderLevels,
+	SyncAllSubsurfRenderLevels,
+	RenderResolutionPercentageMenu,
+	SimplifyRenderMenu,
+	SlotsRenderMenu,
+	ShadeingMenu,
+	SubsurfMenu
+]
+
+def register():
+	for cls in classes:
+		bpy.utils.register_class(cls)
+
+def unregister():
+	for cls in classes:
+		bpy.utils.unregister_class(cls)
+
+
+################
 # メニュー追加 #
 ################
 
 # メニューのオン/オフの判定
 def IsMenuEnable(self_id):
-	for id in bpy.context.user_preferences.addons["Scramble Addon"].preferences.disabled_menu.split(','):
+	for id in bpy.context.preferences.addons["Blender-Scramble-Addon-master"].preferences.disabled_menu.split(','):
 		if (id == self_id):
 			return False
 	else:
@@ -244,6 +270,6 @@ def menu(self, context):
 		self.layout.prop(context.scene.world.light_settings, 'samples', text="AO Samples", icon="PLUGIN")
 		self.layout.separator()
 		self.layout.menu(SimplifyRenderMenu.bl_idname, icon="PLUGIN")
-	if (context.user_preferences.addons["Scramble Addon"].preferences.use_disabled_menu):
+	if (context.preferences.addons["Blender-Scramble-Addon-master"].preferences.use_disabled_menu):
 		self.layout.separator()
 		self.layout.operator('wm.toggle_menu_enable', icon='CANCEL').id = __name__.split('.')[-1]
