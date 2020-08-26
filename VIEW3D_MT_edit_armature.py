6a5483aac916b5d45248cca75bd07a0748397ff3
nikogoli <49331838+nikogoli@users.noreply.github.com>
2020-08-26 11:57:27 +0900

    Update VIEW3D_MT_edit_armature.py
    各サブモジュールにおいて
    
    ・追加先の変更に合わせ、コメントを変更
    
    ・クラスの登録・登録解除の関数を追加：
    　register(), unregister()
    
    ・プリファレンスのRNA名変更への対処：
    　contxt.user_preferences　→　context.preferences
    
    ・addon名の検索失敗への対処：
    　Scramble Addon　→　Blender-Scramble-Addon-master

--- VIEW3D_MT_edit_armature.py
+++ VIEW3D_MT_edit_armature.py
@@ -33,12 +33,30 @@ class ShortcutMenu(bpy.types.Menu):
 		self.layout.operator(DeleteUnmassage.bl_idname, icon='PLUGIN')
 
 ################
+# クラスの登録 #
+################
+
+classes = [
+	DeleteUnmassage,
+	ShortcutMenu
+]
+
+def register():
+	for cls in classes:
+		bpy.utils.register_class(cls)
+
+def unregister():
+	for cls in classes:
+		bpy.utils.unregister_class(cls)
+
+
+################
 # メニュー追加 #
 ################
 
 # メニューのオン/オフの判定
 def IsMenuEnable(self_id):
-	for id in bpy.context.user_preferences.addons["Scramble Addon"].preferences.disabled_menu.split(','):
+	for id in bpy.context.preferences.addons["Blender-Scramble-Addon-master"].preferences.disabled_menu.split(','):
 		if (id == self_id):
 			return False
 	else:
@@ -49,6 +67,6 @@ def menu(self, context):
 	if (IsMenuEnable(__name__.split('.')[-1])):
 		self.layout.separator()
 		self.layout.menu(ShortcutMenu.bl_idname, icon='PLUGIN')
-	if (context.user_preferences.addons["Scramble Addon"].preferences.use_disabled_menu):
+	if (context.preferences.addons["Blender-Scramble-Addon-master"].preferences.use_disabled_menu):
 		self.layout.separator()
 		self.layout.operator('wm.toggle_menu_enable', icon='CANCEL').id = __name__.split('.')[-1]
