81b281bb67a8f2b321ca534b2d083a6ec19cb5ce
nikogoli <49331838+nikogoli@users.noreply.github.com>
2020-08-26 11:55:55 +0900

    Update PHYSICS_PT_dynamic_paint.py
    各サブモジュールにおいて
    
    ・追加先の変更に合わせ、コメントを変更
    
    ・クラスの登録・登録解除の関数を追加：
    　register(), unregister()
    
    ・プリファレンスのRNA名変更への対処：
    　contxt.user_preferences　→　context.preferences
    
    ・addon名の検索失敗への対処：
    　Scramble Addon　→　Blender-Scramble-Addon-master

--- PHYSICS_PT_dynamic_paint.py
+++ PHYSICS_PT_dynamic_paint.py
@@ -85,12 +85,29 @@ class copy_dynamic_paint(bpy.types.Operator):
 		return {'FINISHED'}
 
 ################
+# クラスの登録 #
+################
+
+classes = [
+	copy_dynamic_paint
+]
+
+def register():
+	for cls in classes:
+		bpy.utils.register_class(cls)
+
+def unregister():
+	for cls in classes:
+		bpy.utils.unregister_class(cls)
+
+
+################
 # メニュー追加 #
 ################
 
 # メニューのオン/オフの判定
 def IsMenuEnable(self_id):
-	for id in bpy.context.user_preferences.addons["Scramble Addon"].preferences.disabled_menu.split(','):
+	for id in bpy.context.preferences.addons["Blender-Scramble-Addon-master"].preferences.disabled_menu.split(','):
 		if (id == self_id):
 			return False
 	else:
@@ -100,5 +117,5 @@ def IsMenuEnable(self_id):
 def menu(self, context):
 	if (IsMenuEnable(__name__.split('.')[-1])):
 		self.layout.operator(copy_dynamic_paint.bl_idname, icon='COPY_ID')
-	if (context.user_preferences.addons["Scramble Addon"].preferences.use_disabled_menu):
+	if (context.preferences.addons["Blender-Scramble-Addon-master"].preferences.use_disabled_menu):
 		self.layout.operator('wm.toggle_menu_enable', icon='CANCEL').id = __name__.split('.')[-1]
