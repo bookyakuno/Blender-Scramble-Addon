8b589bcf7b79eb56ff2fcbc32ac43ba1551a8ce8
nikogoli <49331838+nikogoli@users.noreply.github.com>
2020-08-26 11:57:39 +0900

    Update VIEW3D_MT_edit_mesh_delete.py
    各サブモジュールにおいて
    
    ・追加先の変更に合わせ、コメントを変更
    
    ・クラスの登録・登録解除の関数を追加：
    　register(), unregister()
    
    ・プリファレンスのRNA名変更への対処：
    　contxt.user_preferences　→　context.preferences
    
    ・addon名の検索失敗への対処：
    　Scramble Addon　→　Blender-Scramble-Addon-master

--- VIEW3D_MT_edit_mesh_delete.py
+++ VIEW3D_MT_edit_mesh_delete.py
@@ -50,12 +50,30 @@ class DeleteHideMesh(bpy.types.Operator):
 		return {'FINISHED'}
 
 ################
+# クラスの登録 #
+################
+
+classes = [
+	DeleteBySelectMode,
+	DeleteHideMesh
+]
+
+def register():
+	for cls in classes:
+		bpy.utils.register_class(cls)
+
+def unregister():
+	for cls in classes:
+		bpy.utils.unregister_class(cls)
+
+
+################
 # メニュー追加 #
 ################
 
 # メニューのオン/オフの判定
 def IsMenuEnable(self_id):
-	for id in bpy.context.user_preferences.addons["Scramble Addon"].preferences.disabled_menu.split(','):
+	for id in bpy.context.preferences.addons["Blender-Scramble-Addon-master"].preferences.disabled_menu.split(','):
 		if (id == self_id):
 			return False
 	else:
@@ -68,6 +86,6 @@ def menu(self, context):
 		self.layout.operator(DeleteBySelectMode.bl_idname, icon="PLUGIN")
 		self.layout.operator(DeleteHideMesh.bl_idname, icon="PLUGIN")
 		self.layout.operator('mesh.dissolve_mode')
-	if (context.user_preferences.addons["Scramble Addon"].preferences.use_disabled_menu):
+	if (context.preferences.addons["Blender-Scramble-Addon-master"].preferences.use_disabled_menu):
 		self.layout.separator()
 		self.layout.operator('wm.toggle_menu_enable', icon='CANCEL').id = __name__.split('.')[-1]
