1eec744f846198f005b3af5b56a5c5b2f015f525
nikogoli <49331838+nikogoli@users.noreply.github.com>
2020-08-26 11:56:44 +0900

    Update TEXT_MT_text.py
    各サブモジュールにおいて
    
    ・追加先の変更に合わせ、コメントを変更
    
    ・クラスの登録・登録解除の関数を追加：
    　register(), unregister()
    
    ・プリファレンスのRNA名変更への対処：
    　contxt.user_preferences　→　context.preferences
    
    ・addon名の検索失敗への対処：
    　Scramble Addon　→　Blender-Scramble-Addon-master

--- TEXT_MT_text.py
+++ TEXT_MT_text.py
@@ -26,13 +26,30 @@ class ExternalEdit(bpy.types.Operator):
 	def execute(self, context):
 		path = bpy.path.abspath(context.edit_text.filepath)
 		if (self.index == 1):
-			editor_path = context.user_preferences.addons["Scramble Addon"].preferences.text_editor_path_1
+			editor_path = context.preferences.addons["Blender-Scramble-Addon-master"].preferences.text_editor_path_1
 		elif (self.index == 2):
-			editor_path = context.user_preferences.addons["Scramble Addon"].preferences.text_editor_path_2
+			editor_path = context.preferences.addons["Blender-Scramble-Addon-master"].preferences.text_editor_path_2
 		elif (self.index == 3):
-			editor_path = context.user_preferences.addons["Scramble Addon"].preferences.text_editor_path_3
+			editor_path = context.preferences.addons["Blender-Scramble-Addon-master"].preferences.text_editor_path_3
 		subprocess.Popen([editor_path, path])
 		return {'FINISHED'}
+
+################
+# クラスの登録 #
+################
+
+classes = [
+	ExternalEdit
+]
+
+def register():
+	for cls in classes:
+		bpy.utils.register_class(cls)
+
+def unregister():
+	for cls in classes:
+		bpy.utils.unregister_class(cls)
+
 
 ################
 # メニュー追加 #
@@ -40,7 +57,7 @@ class ExternalEdit(bpy.types.Operator):
 
 # メニューのオン/オフの判定
 def IsMenuEnable(self_id):
-	for id in bpy.context.user_preferences.addons["Scramble Addon"].preferences.disabled_menu.split(','):
+	for id in bpy.context.preferences.addons["Blender-Scramble-Addon-master"].preferences.disabled_menu.split(','):
 		if (id == self_id):
 			return False
 	else:
@@ -50,18 +67,18 @@ def IsMenuEnable(self_id):
 def menu(self, context):
 	if (IsMenuEnable(__name__.split('.')[-1])):
 		self.layout.separator()
-		if (context.user_preferences.addons["Scramble Addon"].preferences.text_editor_path_1):
-			path = os.path.basename(context.user_preferences.addons["Scramble Addon"].preferences.text_editor_path_1)
+		if (context.preferences.addons["Blender-Scramble-Addon-master"].preferences.text_editor_path_1):
+			path = os.path.basename(context.preferences.addons["Blender-Scramble-Addon-master"].preferences.text_editor_path_1)
 			name, ext = os.path.splitext(path)
 			self.layout.operator(ExternalEdit.bl_idname, icon="PLUGIN", text=name+"  Open").index = 1
-		if (context.user_preferences.addons["Scramble Addon"].preferences.text_editor_path_2):
-			path = os.path.basename(context.user_preferences.addons["Scramble Addon"].preferences.text_editor_path_2)
+		if (context.preferences.addons["Blender-Scramble-Addon-master"].preferences.text_editor_path_2):
+			path = os.path.basename(context.preferences.addons["Blender-Scramble-Addon-master"].preferences.text_editor_path_2)
 			name, ext = os.path.splitext(path)
 			self.layout.operator(ExternalEdit.bl_idname, icon="PLUGIN", text=name+"  Open").index = 2
-		if (context.user_preferences.addons["Scramble Addon"].preferences.text_editor_path_3):
-			path = os.path.basename(context.user_preferences.addons["Scramble Addon"].preferences.text_editor_path_3)
+		if (context.preferences.addons["Blender-Scramble-Addon-master"].preferences.text_editor_path_3):
+			path = os.path.basename(context.preferences.addons["Blender-Scramble-Addon-master"].preferences.text_editor_path_3)
 			name, ext = os.path.splitext(path)
 			self.layout.operator(ExternalEdit.bl_idname, icon="PLUGIN", text=name+"  Open").index = 3
-	if (context.user_preferences.addons["Scramble Addon"].preferences.use_disabled_menu):
+	if (context.preferences.addons["Blender-Scramble-Addon-master"].preferences.use_disabled_menu):
 		self.layout.separator()
 		self.layout.operator('wm.toggle_menu_enable', icon='CANCEL').id = __name__.split('.')[-1]
