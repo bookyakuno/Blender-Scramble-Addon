fc045812297e46b82f3116bd76727c4d4c0ca8e4
bookyakuo <kiokuya@gmail.com>
2020-09-01 18:51:07 +0900

    __init__ アドオン設定のメニューを改善。

--- __init__.py
+++ __init__.py
@@ -218,8 +218,15 @@ import bpy
 class AddonPreferences(bpy.types.AddonPreferences):
 	bl_idname = __name__
 
+	tab_addon_menu            : EnumProperty(name="Tab", description="", items=[
+	('DISPLAY', "Display", ""),
+	 # ('KEYMAP', "Keymap", ""),
+	('EXTERNAL_APP', "External app", ""),
+	('LINK', "Link", "")
+	], default='DISPLAY')
+
 	disabled_menu : StringProperty(name="Invalid Menu", default="")
-	use_disabled_menu : BoolProperty(name="\"On/Off additional items\" hidden", default=False)
+	use_disabled_menu : BoolProperty(name="Enable 'On/Off additional items' Button", default=False)
 	view_savedata : StringProperty(name="View Save Data", default="")
 	key_config_xml_path : StringProperty(name="XML Config Path", default="BlenderKeyConfig.xml")
 
@@ -233,18 +240,38 @@ class AddonPreferences(bpy.types.AddonPreferences):
 
 	def draw(self, context):
 		layout = self.layout
-		layout.prop(self, 'disabled_menu')
-		layout.prop(self, 'use_disabled_menu')
-		layout.prop(self, 'view_savedata')
-		layout.prop(self, 'key_config_xml_path')
-		box = layout.box()
-		box.prop(self, 'image_editor_path_1')
-		box.prop(self, 'image_editor_path_2')
-		box.prop(self, 'image_editor_path_3')
-		box = layout.box()
-		box.prop(self, 'text_editor_path_1')
-		box.prop(self, 'text_editor_path_2')
-		box.prop(self, 'text_editor_path_3')
+
+		row = layout.row(align=True)
+		row.prop(self,"tab_addon_menu",expand=True)
+
+		if self.tab_addon_menu == "DISPLAY":
+			box = layout.box()
+			box.prop(self, 'use_disabled_menu')
+			box.prop(self, 'disabled_menu')
+			box = layout.box()
+			box.prop(self, 'view_savedata')
+			box = layout.box()
+			box.prop(self, 'key_config_xml_path')
+
+
+		elif self.tab_addon_menu == "EXTERNAL_APP":
+			box = layout.box()
+			box.label(text="Image Editor",icon="IMAGE")
+			box.prop(self, 'image_editor_path_1')
+			box.prop(self, 'image_editor_path_2')
+			box.prop(self, 'image_editor_path_3')
+			box = layout.box()
+			box.label(text="Eext Editoer",icon="TEXT")
+			box.prop(self, 'text_editor_path_1')
+			box.prop(self, 'text_editor_path_2')
+			box.prop(self, 'text_editor_path_3')
+		elif self.tab_addon_menu == "LINK":
+			box = layout.box()
+			box.operator(
+				"wm.url_open", text="Github", icon="URL"
+			).url = "https://github.com/bookyakuno/Blender-Scramble-Addon"
+
+
 
 # 追加メニューの有効/無効
 class ToggleMenuEnable(bpy.types.Operator):
@@ -283,8 +310,9 @@ def GetTranslationDict():
 		reader = csv.reader(f)
 		dict['ja_JP'] = {}
 		for row in reader:
-			for context in bpy.app.translations.contexts:
-				dict['ja_JP'][(context, row[1])] = row[0]
+			if row:
+				for context in bpy.app.translations.contexts:
+					dict['ja_JP'][(context, row[1])] = row[0]
 		"""
 		for lang in bpy.app.translations.locales:
 			if (lang == 'ja_JP'):
@@ -298,7 +326,8 @@ def GetTranslationDict():
 
 
 classes = [
-	AddonPreferences
+	ToggleMenuEnable,
+	AddonPreferences,
 ]
 
 # プラグインをインストールしたときの処理
