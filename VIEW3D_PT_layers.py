65934a72243b5caed224eb4ebec965c2febea5a1
nikogoli <49331838+nikogoli@users.noreply.github.com>
2020-10-03 18:18:48 +0900

    機能調整(ほぼ完了)：VIEW3D_PT_layers
    ○対象をレイヤーではなくコレクションに変更
    
    機能：
    「ビュー」サイドバーにあるデフォルトのコレクション表示を基礎にして
    ・アクティブなオブジェクトを含むコレクションのアイコンをわかりやすいものに変更
    
    ・コレクションの並び方を親子関係に基づくものに変更
    
    ・ワイヤー表示機能を追加
    
    ※処理は VIEW3D_MT_view.py の流用なのでそちらを参照のこと
    ◆ VIEW3D_MT_view.py と同様に、もともとある ops.objects.hide_collection の利用を検討中
    
    ・サイドバーに登録する都合上 menu 関数が必要：
    　→　パネルクラスに相当する内容を menu に記載
    
    ・デフォルトのコレクションパネルに合わせ、名前部分(選択以外を非表示)と目玉アイコン(選択の表示を切り替え)の2つに分けてオペレーターを登録

--- VIEW3D_PT_layers.py
+++ VIEW3D_PT_layers.py
@@ -8,86 +8,84 @@ from bpy.props import *
 # オペレーター #
 ################
 
-class GroupLayers(bpy.types.Operator):
-	bl_idname = "object.group_layers"
-	bl_label = "Toggle Show/Hide Groups"
-	bl_description = "Switch Show / Hide group has"
-	bl_options = {'REGISTER', 'UNDO'}
+class CollectionShowHide(bpy.types.Operator): #
+	bl_idname = "view3d.collection_show_hide"
+	bl_label = "Toggle Show/Hide Collections"
+	bl_description = "Shows or hides collection"
+	bl_options = {'REGISTER'}
 
-	group : StringProperty(name="Group Name")
+	name : StringProperty(name="Collection Name")
+	parent_names : StringProperty(name="Parent-Collections\' Names")
+	exclusive : BoolProperty(name="Hide Others", default=False)
+	wire : BoolProperty(name="Wireframe", default=False)
 
 	def execute(self, context):
-		for obj in bpy.data.objects:
-			for l1 in obj.layers:
-				for l2 in context.scene.layers:
-					if (l1 and l2):
-						if (self.group != ""):
-							for group in obj.users_group:
-								if (group.name == self.group):
-									obj.hide = False
-									break
-							else:
-								obj.hide = True
-						else:
-							if (len(obj.users_group) == 0):
-								obj.hide = False
-							else:
-								obj.hide = True
+		par_names = [x for x in self.parent_names.split(",") if not len(x) == 0]
+		if not par_names:
+			coll = context.view_layer.layer_collection.children[self.name]
+			if (self.exclusive):
+				for col in context.view_layer.layer_collection.children:
+					if col.name != self.name: col.hide_viewport = True
+		else:
+			par = context.view_layer.layer_collection.children[par_names[0]]
+			if (self.exclusive):
+				for col in context.view_layer.layer_collection.children:
+					if col.name != par_names[0]: col.hide_viewport = True
+			try:
+				for name in par_names[1:]:
+					if (self.exclusive):
+						for col in par.children:
+							if col.name != name: col.hide_viewport = True
+					par = par.children[name]
+			except IndexError:
+				pass		
+			coll = par.children[self.name]
+		if (self.exclusive):
+			coll.hide_viewport = False
+			return {'FINISHED'}
+		if (self.wire):
+			for obj in coll.collection.objects:
+				obj.show_all_edges = True
+				if obj.display_type != 'WIRE':
+					obj.display_type = 'WIRE'
+				else:
+					obj.display_type = 'TEXTURED'
+		else:
+			coll.hide_viewport = not coll.hide_viewport
 		return {'FINISHED'}
+	def invoke(self, context, event):
+		if (event.shift):
+			self.exclusive = False
+			self.wire = True
+		return self.execute(context)
 
-##########
-# パネル #
-##########
+def flatten(layer_collection, parent_name=""):
+	flat = []
+	for coll in layer_collection.children:
+		if len(coll.children) > 0:
+			flat.append((coll, f"{parent_name},{layer_collection.name}"))
+			flat += flatten(coll, f"{parent_name},{layer_collection.name}")
+		else:
+			flat.append((coll, f"{parent_name},{layer_collection.name}"))
+			flat.append((None, None))
+	return flat
 
-class ObjectSelectPanel(bpy.types.Panel):
-	bl_idname = "VIEW3D_PT_layers"
-	bl_label = "ObjectSelectPanel"
-	bl_space_type = 'VIEW_3D'
-	bl_region_type = 'UI'
-	bl_options = {'DEFAULT_CLOSED'}
+def GetIcon(layer_collection):
+	if layer_collection.hide_viewport:
+		return "HIDE_ON"
+	for obj in layer_collection.collection.objects[:5]:
+		if (obj.display_type != 'WIRE'):
+			return "HIDE_OFF"
+	else:
+		return 'SHADING_WIRE'
 
-	@classmethod
-	def poll(cls, context):
-		return (context.object is not None)
-#
-#	def draw_header(self, context):
-#		row = self.layout.row()
-#		row.scale_x = 0.7
-#		row.scale_y = 0.7
-#		row.prop(context.scene, 'layers', text="")
-#
-	def draw(self, context):
-		if (context.object):
-			if (context.object.type == 'ARMATURE'):
-				self.layout.label(text="Bone Layer")
-				col = self.layout.column()
-				col.scale_y = 0.7
-				col.prop(context.object.data, 'layers', text="")
-		self.layout.label(text="Layers by Groups")
-		objs = []
-		for obj in bpy.data.objects:
-			for i in range(len(obj.layers)):
-				if (obj.layers[i] and context.scene.layers[i]):
-					objs.append(obj)
-		groups = []
-		for obj in objs:
-			for group in obj.users_group:
-				if (not group in groups):
-					groups.append(group)
-		row = self.layout.row(align=True)
-		row.operator('object.hide_view_clear', text="Show All", icon='RESTRICT_VIEW_OFF')
-		row.operator(GroupLayers.bl_idname, text="Non-Group", icon='FILE').group = ''
-		col = self.layout.column(align=True)
-		for group in groups:
-			col.operator(GroupLayers.bl_idname, text=group.name, icon='PLUGIN').group = group.name
 
 ################
 # クラスの登録 #
 ################
 
 classes = [
-	GroupLayers,
-	ObjectSelectPanel
+	CollectionShowHide
 ]
 
 def register():
@@ -103,6 +101,59 @@ def unregister():
 # メニュー追加 #
 ################
 
+# メニューのオン/オフの判定
+def IsMenuEnable(self_id):
+	for id in bpy.context.preferences.addons[__name__.partition('.')[0]].preferences.disabled_menu.split(','):
+		if (id == self_id):
+			return False
+	else:
+		return True
+
 # メニューを登録する関数
 def menu(self, context):
-	pass
+	if (IsMenuEnable(__name__.split('.')[-1])):
+		#coll_dic = {c.name:  for }
+		if (context.object):
+			if (context.object.type == 'ARMATURE'):
+				self.layout.label(text="Bone Layer")
+				col = self.layout.column()
+				col.scale_y = 0.7
+				col.prop(context.object.data, 'layers', text="")
+		self.layout.separator(factor=1.0)
+		if context.active_object:
+			obj_par_collection = context.active_object.users_collection[0].name
+		else:
+			obj_par_collection = ""
+		for col in context.view_layer.layer_collection.children:
+			row = self.layout.row()
+			if col.name == obj_par_collection:
+				row.label(icon='KEYTYPE_KEYFRAME_VEC')
+			else:
+				row.label(icon='HANDLETYPE_AUTO_VEC')
+			op1 = row.operator(CollectionShowHide.bl_idname, text=f"{col.name}", icon='NONE', emboss=False)
+			op1.name, op1.parent_names = [col.name, ""]
+			op1.exclusive, op1.wire = [True, False]
+			op2 = row.operator(CollectionShowHide.bl_idname, text="", icon=GetIcon(col), emboss=False)
+			op2.name, op2.parent_names = [col.name, ""]
+			op2.exclusive, op2.wire = [False, False]
+			flatten_nest = flatten(col)[:-1]
+			for coll in flatten_nest:
+				if coll[0] == None:
+					self.layout.separator(factor=0.3)
+				else:
+					row = self.layout.row()
+					if coll[0].name == obj_par_collection:
+						row.label(icon='KEYTYPE_KEYFRAME_VEC')
+					else:
+						row.label(icon='HANDLETYPE_AUTO_VEC')				
+					op1 = row.operator(CollectionShowHide.bl_idname, text=f"{coll[0].name}", icon='NONE', emboss=False)
+					op1.name, op1.parent_names = [coll[0].name, coll[1]]
+					op1.exclusive, op1.wire = [True, False]
+					op2 = row.operator(CollectionShowHide.bl_idname, text="", icon=GetIcon(coll[0]), emboss=False)
+					op2.name, op2.parent_names = [coll[0].name, coll[1]]
+					op2.exclusive, op2.wire = [False, False]
+			self.layout.separator(factor=1.0)
+		self.layout.label(text="(shift: Toggle Wireframe)")
+
+	if (context.preferences.addons[__name__.partition('.')[0]].preferences.use_disabled_menu):
+		self.layout.operator('wm.toggle_menu_enable', icon='CANCEL').id = __name__.split('.')[-1]
