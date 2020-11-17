471adabf4945c3cf5a35f454bdbf4da2ebe0ed35
nikogoli <49331838+nikogoli@users.noreply.github.com>
2020-11-18 01:40:18 +0900

    機能調整：DOPESHEET_MT_key
    ◇「重複するキーフレームを削除」機能
    ・処理の対象とするアクションの範囲を変更：
    　全てのアクション　→　表示状態にあるオブジェクトのアクション
    
    ・重複の判断方法を変更：
    　前後両方と値が近い　→　前後どちらかと値が近い
    
    ・上記の変更に伴い、アクションの両端での比較対象の設定方法を変更：
    　前後どちらかしかキーがない場合、そのキーに対して2回重複を判断する
    
    ・「キーを1つ残す」処理の変更：
    　アクションのキーがすべて消える場合に1つを残す
    　→　削除対象から1つ取り除く
    
    ・上記の変更により、アクションが削除される場合が限定できるようになったので、削除の条件を変更
    
    ・Fカーブモディファイアがある場合にエラー表示を行う処理を追加

--- DOPESHEET_MT_key.py
+++ DOPESHEET_MT_key.py
@@ -29,7 +29,9 @@ class CreanEX(bpy.types.Operator):
 
 	def execute(self, context):
 		threshold = self.threshold
-		for action in bpy.data.actions[:]:
+		animations = [ob.animation_data for ob in bpy.data.objects if ob.hide_get() == False]
+		action_datas = [an.action for an in animations if an != None and an.action != None]
+		for action in action_datas:
 			for fcurve in action.fcurves[:]:
 				if (not fcurve.modifiers):
 					delete_points = []
@@ -38,23 +40,30 @@ class CreanEX(bpy.types.Operator):
 						if (0 < i):
 							pre_point = fcurve.keyframe_points[i-1].co[1]
 						else:
-							pre_point = now_point
+							#pre_point = now_point
+							pre_point = fcurve.keyframe_points[i+1].co[1]
 						try:
 							next_point = fcurve.keyframe_points[i+1].co[1]
 						except IndexError:
-							next_point = now_point
-						if (now_point-threshold <= pre_point <= now_point+threshold):
-							if (pre_point-threshold <= next_point <= pre_point+threshold):
-								handle_left = fcurve.keyframe_points[i].handle_left[1]
-								handle_right = fcurve.keyframe_points[i].handle_right[1]
-								if (handle_left-threshold <= handle_right <= handle_left+threshold):
-									delete_points.append(fcurve.keyframe_points[i])
+							#next_point = now_point
+							next_point = fcurve.keyframe_points[i-1].co[1]
+						now_pre = (now_point-threshold <= pre_point <= now_point+threshold)
+						now_next = (now_point-threshold <= next_point <= now_point+threshold)
+						#pre_next = (pre_point-threshold <= next_point <= pre_point+threshold)
+						if (now_pre or now_next):#(now_pre and pre_next):
+							handle_left = fcurve.keyframe_points[i].handle_left[1]
+							handle_right = fcurve.keyframe_points[i].handle_right[1]
+							if (handle_left-threshold <= handle_right <= handle_left+threshold):
+								delete_points.append(fcurve.keyframe_points[i])
+					if self.keep_fcurves:
+						delete_points = delete_points[:-1]
 					for point in delete_points:
-						if (self.keep_fcurves and len(fcurve.keyframe_points) <= 1):
-							break
 						fcurve.keyframe_points.remove(point)
-					if (len(fcurve.keyframe_points) <= 1 and not self.keep_fcurves):
+					if len(fcurve.keyframe_points) == 0:
 						action.fcurves.remove(fcurve)
+				else:
+					nam = action.name + "'s " + fcurve.data_path + " "
+					self.report(type={'ERROR'}, message= nam + "is ignored because it has f-curve modifiers")
 		for area in context.screen.areas:
 			area.tag_redraw()
 		return {'FINISHED'}
