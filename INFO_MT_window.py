a34c6691eadc4c787c034f1d2603b3033e3f66a7
nikogoli <49331838+nikogoli@users.noreply.github.com>
2020-11-28 20:01:58 +0900

    機能調整：INFO_MT_window
    ○共通する変更
    ・トップバーでの使用時のエラー処理を追加
    ※ルートのパイメニュー表示の時点で処理するため、実質的に3つのメニューでの処理として機能
    
    ・UV/画像やノード関連の分割のため、mode を指定する処理を追加
    
    ・並び替え
    ・パイメニュー内の位置をコメントに追加
    
    ◇「エディター切り替え」パイメニュー
    ・UVエディターと画像エディターを分割
    ・ノードエディターをコンポジターとテクスチャノード、マテリアルノードに分割
    ・上記の変更に伴い、アウトライナー、テキストエディター、プロパティを別メニューに移動
    
    ◇「アニメーション関連のエディター切り替え」パイメニュー
    ・ビデオシーケンスエディター、動画クリップエディターをここに移動
    ・ドライバー編集を追加
    
    ◇「その他のエディター切り替え」パイメニュー
    ・ビデオシーケンスエディター、動画クリップエディターを別メニューに移動
    ・テキストエディター、アウトライナー、プロパティをここに移動
    
    ◇実際の切り替え処理
    ・UV/画像やノード関連の分割のため、各スペースの mode を設定する処理を追加

--- INFO_MT_window.py
+++ INFO_MT_window.py
@@ -23,6 +23,9 @@ class AreaTypePieOperator(bpy.types.Operator):
 	bl_options = {'REGISTER', 'UNDO'}
 
 	def execute(self, context):
+		if context.area.type == 'TOPBAR':
+			self.report(type={'ERROR'}, message="This is 'Topbar'. Please use in other areas (by using shortcut)")
+			return {'CANCELLED'}
 		bpy.ops.wm.call_menu_pie(name=AreaTypePie.bl_idname)
 		return {'FINISHED'}
 class AreaTypePie(bpy.types.Menu): #
@@ -31,15 +34,28 @@ class AreaTypePie(bpy.types.Menu): #
 	bl_description = "This is pie menu of editor type change"
 
 	def draw(self, context):
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Text Editor", icon="TEXT").type = "TEXT_EDITOR"
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Outliner", icon="OUTLINER").type = "OUTLINER"
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Property", icon="PROPERTIES").type = "PROPERTIES"
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="3D View", icon="VIEW3D").type = "VIEW_3D"
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="UV/Image Editor", icon="IMAGE").type = "IMAGE_EDITOR"
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Node Editor", icon="NODE_MATERIAL").type = "NODE_EDITOR"
-		self.layout.menu_pie().operator("wm.call_menu_pie", text="Anime", icon="ACTION").name = AreaTypePieAnim.bl_idname
+		#Left
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Compositor", icon="NODE_COMPOSITING")
+		op.type, op.mode = ["NODE_EDITOR", "CompositorNodeTree"]
+		#Right
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Image Editor", icon="IMAGE")
+		op.type, op.mode = ["IMAGE_EDITOR", "VIEW"]
+		#Bottom
 		self.layout.menu_pie().operator("wm.call_menu_pie", text="Other", icon="QUESTION").name = AreaTypePieOther.bl_idname
-
+		#Top
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="3D Viewport", icon="VIEW3D")
+		op.type, op.mode = ["VIEW_3D", ""]
+		#Top Left
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Shader Editor", icon="NODE_MATERIAL")
+		op.type, op.mode = ["NODE_EDITOR", "ShaderNodeTree"]
+		#Top Right
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="UV Editor", icon="UV")
+		op.type, op.mode = ["IMAGE_EDITOR", "UV"]
+		#Bottom Left
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Texture Node Editor", icon="NODE_TEXTURE")
+		op.type, op.mode = ["NODE_EDITOR", "TextureNodeTree"]
+		#Bottom Right
+		self.layout.menu_pie().operator("wm.call_menu_pie", text="Animetion-related", icon="ACTION").name = AreaTypePieAnim.bl_idname
 
 class AreaTypePieAnim(bpy.types.Menu):
 	bl_idname = "INFO_MT_window_pie_area_type_anim"
@@ -47,23 +63,49 @@ class AreaTypePieAnim(bpy.types.Menu):
 	bl_description = "Is pie menu change editor type (animation related)"
 
 	def draw(self, context):
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="NLA Editor", icon="NLA").type = "NLA_EDITOR"
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="DopeSheet", icon="ACTION").type = "DOPESHEET_EDITOR"
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Graph Editor", icon="GRAPH").type = "GRAPH_EDITOR"
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Timeline", icon="TIME").type = "TIMELINE"
+		#Left
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Graph Editor", icon="GRAPH")
+		op.type, op.mode = ["GRAPH_EDITOR", "FCURVES"]
+		#Right
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="DopeSheet", icon="ACTION")
+		op.type, op.mode = ["DOPESHEET_EDITOR", ""]
+		#Bottom
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Video Clip Editor", icon="TRACKER")
+		op.type, op.mode = ["CLIP_EDITOR", ""]
+		#Top
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Video Sequence Editor", icon="SEQUENCE")
+		op.type, op.mode = ["SEQUENCE_EDITOR", ""]
+		#Top Left
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Timeline", icon="TIME")
+		op.type, op.mode = ["TIMELINE", ""]
+		#Top Right
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Non-Liner Animation", icon="NLA")
+		op.type, op.mode = ["NLA_EDITOR", ""]
+		#Left Bottom
+		op = self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Drivers", icon="DRIVER")
+		op.type, op.mode = ["GRAPH_EDITOR", "DRIVERS"]
 class AreaTypePieOther(bpy.types.Menu):
 	bl_idname = "INFO_MT_window_pie_area_type_other"
 	bl_label = "Editor Type (other)"
 	bl_description = "Is pie menu change editor type (other)"
 
 	def draw(self, context):
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Topbar", icon="BLENDER").type = "TOPBAR"
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Video Sequence Editor", icon="SEQUENCE").type = "SEQUENCE_EDITOR"
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Video Clip Editor", icon="TRACKER").type = "CLIP_EDITOR"
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="File Browser", icon="FILEBROWSER").type = "FILE_BROWSER"
+		#Left
+		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Preferences", icon="PREFERENCES").type = "PREFERENCES"
+		#Right
+		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Outliner", icon="OUTLINER").type = "OUTLINER"
+		#Bottom
 		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Python Console", icon="CONSOLE").type = "CONSOLE"
+		#Top
 		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Info", icon="INFO").type = "INFO"
-		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="User Setting", icon="PREFERENCES").type = "USER_PREFERENCES"
+		#Top Left
+		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Topbar", icon="BLENDER").type = "TOPBAR"
+		#Top Right
+		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Property", icon="PROPERTIES").type = "PROPERTIES"
+		#Bottom Left
+		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="File Browser", icon="FILEBROWSER").type = "FILE_BROWSER"
+		#Bottom Right
+		self.layout.menu_pie().operator(SetAreaType.bl_idname, text="Text Editor", icon="TEXT").type = "TEXT_EDITOR"
 class SetAreaType(bpy.types.Operator): #
 	bl_idname = "wm.set_area_type"
 	bl_label = "Change Editor Type"
@@ -71,9 +113,14 @@ class SetAreaType(bpy.types.Operator): #
 	bl_options = {'REGISTER'}
 
 	type : StringProperty(name="Area Type")
+	mode : StringProperty(name="Space Mode", default="")
 
 	def execute(self, context):
 		context.area.type = self.type
+		if (self.type != 'NODE_EDITOR') and len(self.mode):
+			context.area.spaces[0].mode = self.mode
+		elif (self.type == 'NODE_EDITOR') and len(self.mode):
+			context.area.spaces[0].tree_type = self.mode
 		return {'FINISHED'}
 
 ################
